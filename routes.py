bb91e21b6b0513f6a4442878efef2ad2d41d6c1a
Jean Carraro <jgcarraro@hugtak.com>
2024-03-05 11:56:42 -0300

    finaliza transfer_file

--- routes.py
+++ routes.py
@@ -1,16 +1,34 @@
 class Server:
-    def __init__(self, name: str, previous: str, ip: str, port=22):
+    def __init__(
+        self,
+        name: str,
+        previous: str,
+        user: str,
+        ip: str,
+        port=22,
+        pkey_file="id_rsa",
+    ):
         self.name = name
         self.previous = previous
+        self.user = user
         self.ip = ip
         self.port = port
+        self.pkey_file = f"/home/{user}/.ssh/{pkey_file}"
 
 
 routes = {
-    "vm": Server(name="vm", previous=".", ip="172.17.4.136"),
-    "jump_sp": Server(name="jump_sp", previous="vm", ip="172.19.0.253", port=333),
-    "pr35": Server(name="pr35", previous="jump_sp", ip="172.30.0.35"),
-    "jump_cbc": Server(name="jump_cbc", previous="vm", ip="172.19.253.249"),
-    "cbe1": Server(name="cbe1", previous="jump_cbc", ip="172.17.161.2"),
-    "cbe2": Server(name="cbe2", previous="jump_cbc", ip="172.19.161.2"),
+    "vm": Server(name="vm", previous=".", user="jgcarraro", ip="172.17.4.136"),
+    "jump_sp": Server(
+        name="jump_sp", previous="vm", user="jgcarraro", ip="172.19.0.253", port=333
+    ),
+    "pr35": Server(name="pr35", previous="jump_sp", user="jgcarraro", ip="172.30.0.35"),
+    "jump_cbc": Server(
+        name="jump_cbc", previous="vm", user="jgcarraro", ip="172.19.253.249"
+    ),
+    "cbe1": Server(
+        name="cbe1", previous="jump_cbc", user="jgcarraro", ip="172.17.161.2"
+    ),
+    "cbe2": Server(
+        name="cbe2", previous="jump_cbc", user="jgcarraro", ip="172.19.161.2"
+    ),
 }
--- transfer_file.py
+++ transfer_file.py
@@ -1,3 +1,4 @@
+from io import StringIO
 import sys
 import transfer_file
 import paramiko
@@ -9,7 +10,8 @@ from routes import routes, Server
 def getPathToServer(serverName: str) -> list[Server]:
     targetServer = routes.get(serverName)
     if targetServer is None:
-        raise ValueError(f"Server with name {serverName} not found")
+        print(f"Server with name {serverName} not found")
+        exit(1)
 
     previousName = targetServer.previous
 
@@ -22,10 +24,12 @@ def getPathToServer(serverName: str) -> list[Server]:
 
 def transferFile(file: str, path: list[Server]):
     if not os.path.exists(file):
-        raise ValueError(f"File {file} does not exist")
+        print(f"File {file} does not exist")
+        exit(1)
 
     if len(path) == 0:
-        raise ValueError("Path cannot be empty")
+        print("Path cannot be empty")
+        exit(1)
 
     if len(path) == 1:
         subprocess.run(
@@ -33,29 +37,43 @@ def transferFile(file: str, path: list[Server]):
         )
         return
 
+    basename = os.path.basename(file)
+
     try:
         target = paramiko.SSHClient()
         target.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        target.connect(
-            path[0].ip, port=path[0].port, allow_agent=True, look_for_keys=True
-        )
+        target.connect(path[0].ip, port=path[0].port)
+        target.exec_command("mkdir -p ~/transfered_files")
+        sftp = target.open_sftp()
+        sftp.put(file, f"transfered_files/{basename}")
 
-        for i in range(1, len(path) - 1):
+        for i in range(1, len(path)):
             transport = target.get_transport()
             if transport is None:
-                raise ValueError(f"Failed to connect to {path[i].name}")
+                print(f"Failed to connect to {path[i].name}")
+                exit(1)
+
             channel = transport.open_channel(
                 "direct-tcpip", (path[i].ip, path[i].port), ("localhost", 0)
             )
+
+            sftp = target.open_sftp()
+            with sftp.open(path[i - 1].pkey_file, "r") as key_file:
+                key_data = key_file.read().decode("utf-8")
+                pkey = paramiko.RSAKey.from_private_key(StringIO(key_data))
+
             target = paramiko.SSHClient()
             target.set_missing_host_key_policy(paramiko.AutoAddPolicy())
             target.connect(
                 path[i].ip,
                 port=path[i].port,
-                allow_agent=True,
-                look_for_keys=True,
+                pkey=pkey,
                 sock=channel,
             )
+            target.exec_command("mkdir -p ~/transfered_files")
+            sftp = target.open_sftp()
+            sftp.put(basename, f"transfered_files/{basename}")
+
     finally:
         target.close()
 
@@ -67,8 +85,9 @@ def main():
 
     file = sys.argv[1]
     destination = sys.argv[2]
-
-    print([server.name for server in transfer_file.getPathToServer(destination)])
+    path = transfer_file.getPathToServer(destination)
+    print([server.name for server in path])
+    transferFile(file, path)
 
 
 if __name__ == "__main__":
