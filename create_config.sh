8154cecd3ee32ed62504ad08a6a3c3b2da230fc9
Jean Carraro <jgcarraro@hugtak.com>
2024-03-06 16:54:26 -0300

    modifica scripts

--- create_config.sh
+++ create_config.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
+
 mkdir -p ~/.config/transfer_file
-if [ ! -f "routes.yaml" ]; then
-    echo "arquivo routes.yaml não encontrado"
-    exit 1
-fi
+
 echo "criando arquivo de configuração"
 
 
 # se a flag -f for usada, o arquivo será sobrescrito
-if [[ -f ~/.config/transfer_file/routes.yaml ]] && [[ ! "$1" == "-f" ]]; then
-    echo "arquivo já existe, use a flag -f para sobrescrever"
+if [[ -f ~/.config/transfer_file/routes.yaml ]]; then
+    echo "arquivo já existe"
     exit 1
 fi
-cp routes.yaml ~/.config/transfer_file
+
+cp routes_example.yaml ~/.config/transfer_file/routes.yaml
 echo "arquivo de configuração criado com sucesso"
+echo "adicione rotas em ~/.config/transfer_file/routes.yaml"
--- install.sh
+++ install.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-#check if routes.yaml exists
-if [ ! -f "routes.yaml" ]; then
-    echo "arquivo routes.yaml não encontrado"
-    exit 1
-fi
-
 poetry install
 poetry build -f wheel
 pipx install dist/*.whl --force
+
 ./create_config.sh
 
 echo "Instalação concluída com sucesso!"
